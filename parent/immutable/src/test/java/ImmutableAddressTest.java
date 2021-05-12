59dab12e84b63bd0701aa443d366f88f83150966
czirman <czirman@gmail.com>
2021-05-12 18:32:23 +0200

    Immutable objekt

---
+++ parent/immutable/src/test/java/ImmutableAddressTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+public class ImmutableAddressTest {
+
+    @Test
+    public void tryToChange(){
+        ImmutableAddress ob1 =  new ImmutableAddress("Beata", "Warszawa" );
+        // no i srpóbuj teraz coś zmienić
+    }
+}
