de24869face1c54ea3eb56b193905a74bd9c4e87
czirman <mkucman@gmail.com>
2021-12-10 11:12:59 +0100

    Hashmapy immutable

--- parent/hashmap-collision/src/main/java/immutables/User.java
+++ parent/hashmap-collision/src/main/java/immutables/User.java
@@ -1,25 +1,25 @@
 package immutables;
 
-public class User implements Comparable<User>{
+public class User {
     private String login;
+
     public User(String login) {
         this.login = login;
     }
+
     @Override
     public String toString() {
         return "User{" +
                 "login='" + login + '\'' +
                 '}';
     }
+
     public String getLogin() {
         return login;
     }
+
     public void setLogin(String login) {
         this.login = login;
     }
 
-    @Override
-    public int compareTo(User o) {
-        return 0;
-    }
 }
