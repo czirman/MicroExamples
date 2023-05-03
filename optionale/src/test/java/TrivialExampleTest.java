c1765ad117d252b1cdaa7ce5c6bdd8e2298bfd6c
czirman <perseverance.void@poczta.pl>
2023-05-03 15:39:44 +0200

    Fix optional test

--- optionale/src/test/java/TrivialExampleTest.java
+++ optionale/src/test/java/TrivialExampleTest.java
@@ -1,7 +1,6 @@
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
@@ -11,92 +10,44 @@ import java.util.stream.Stream;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-
 public class TrivialExampleTest {
 
     private final TrivialExample trivialExample = new TrivialExample();
 
     @Test
-    public void useInBadWayWithNoEmptyString(){
+    public void useInBadWayWithNoEmptyString() {
         Optional<String> input = Optional.of(" DanaDana ");
         String result = trivialExample.getTrimBad(input);
-        assertEquals("DanaDana", result  );
+        assertEquals("DanaDana", result);
     }
 
     @Test
-    public void useInBadWayWithNullValue(){
+    public void useInBadWayWithNullValue() {
         Optional<String> input = Optional.ofNullable(null);
         String result = trivialExample.getTrimBad(input);
-        assertEquals("", result  );
+        assertEquals("", result);
     }
 
     @Test
-    public void useInGoodWayWithNoEmptyString(){
+    public void useInGoodWayWithNoEmptyString() {
         String input = " DanaDana ";
         String result = trivialExample.getTrimGood(input);
-        assertEquals("DanaDana", result  );
+        assertEquals("DanaDana", result);
     }
 
     @Test
-    public void useInGoodWayWithNullValue(){
+    public void useInGoodWayWithNullValue() {
         String input = null;
         String result = trivialExample.getTrimGood(input);
-        assertEquals("", result  );
-    }
-
-    @Test
-    public void test(){
-
-        List lista= Arrays.asList("a","b","c","d","e" );
-        Stream stream = lista.stream();
-        stream.map(arg -> arg +"f").collect(Collectors.toList());
-
-        Function ff = f -> f.toString();
-
-    }
-
-    @Test
-    public void test1(){
-
-        Stream<Klasa> stream = Arrays.asList( new Klasa(1),new Klasa(4),new Klasa(2),new Klasa(10),new Klasa(19)).stream();
-        Optional<Integer> b = stream.map(arg -> arg.getHup())
-                .filter(arg -> arg == 2)
-                .peek(arg -> arg =arg+1)
-                .findFirst();
-
-
-        List list = stream.collect(Collectors.toList());
-
-        Klasa ss = new Klasa(1) ;
-
+        assertEquals("", result);
     }
 
     @Test
     public void createStream_whenFindAnyResultIsPresent_thenCorrect() {
-        List<String> list = Arrays.asList("A","B","C","D");
-
+        List<String> list = Arrays.asList("A", "B", "C", "D");
         Optional<String> result = list.stream().findAny();
-
         assertTrue(result.isPresent());
-      // assertThat(result.get(), anyOf(is("A"), is("B"), is("C"), is("D")));
     }
 
 }
 
-class Klasa{
-
-    int hup;
-
-    public Klasa(int hup) {
-        this.hup = hup;
-    }
-
-    public int getHup() {
-        return hup;
-    }
-
-    public void setHup(int hup) {
-        this.hup = hup;
-    }
-
-}
--- streams/src/test/java/SimpleOperation.java
+++ streams/src/test/java/SimpleOperation.java
@@ -1,6 +1,9 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -10,5 +13,25 @@ public class SimpleOperation {
     public void sortPrimitive(){
         List sorted = Stream.of(3,4,6,8,9,1)
                 .sorted().collect(Collectors.toList());
+    }
+
+    // TODO interfejs Function obadać a ten test usunac
+    @Test
+    public void test() {
+        Function ff = f -> f.toString();
+    }
+
+    @Test
+    // TODO wywala wyjatek zwiazany z jedna z waznych cech strumieni
+    public void test1() {
+
+        Stream<Klasa> stream = Arrays.asList(new Klasa(1), new Klasa(4), new Klasa(2), new Klasa(10), new Klasa(19)).stream();
+        Optional<Integer> b = stream.map(arg -> arg.getHup())
+                .filter(arg -> arg == 2)
+                .peek(arg -> arg = arg + 1)
+                .findFirst();
+
+        stream.collect(Collectors.toList());
+
     }
 }
