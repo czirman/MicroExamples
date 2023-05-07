96f5d4f56c073e09499e0cc95b3d485d4d166c58
czirman <perseverance.void@poczta.pl>
2023-05-07 21:41:00 +0200

    Add assertion to test beans mechanizm

--- beans/src/test/java/beans/HowManyBeansTest.java
+++ beans/src/test/java/beans/HowManyBeansTest.java
@@ -4,11 +4,24 @@ import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class HowManyBeansTest {
 
     @Test
     public void howManyBeans() {
         ApplicationContext context = new AnnotationConfigApplicationContext(HowManyBeans.class);
-        context.getBeanDefinitionNames();
+        List<String> beans = Arrays.stream(context.getBeanDefinitionNames())
+                .filter(bean -> bean.matches("storeSouth|storeNorth|storeWest"))
+                .collect(Collectors.toList());
+        assertEquals(3, beans.size());
+        assertEquals("storeSouth", beans.get(0));
+        assertEquals("storeNorth", beans.get(1));
+        assertEquals("storeWest", beans.get(2));
     }
 }
