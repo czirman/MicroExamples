77082c80acb6e86de0fa70c0fbd7dab2b40bef6a
czirman <mkucman@gmail.com>
2023-08-07 17:53:30 +0200

    Refactor

--- beans/src/test/java/beans/HowManyBeansTest.java
+++ beans/src/test/java/beans/HowManyBeansTest.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
