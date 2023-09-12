6cc8c184d093ef0e338a15a5264d3a4ff4aa7392
czirman <mkucman@gmail.com>
2023-09-12 13:01:55 +0200

    Beans Qualifier

--- beans/src/test/java/qualifier/FooServiceTest.java
+++ beans/src/test/java/qualifier/FooServiceTest.java
@@ -1,14 +1,13 @@
 package qualifier;
 
-import automatic.StateBean;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Arrays;
-import java.util.Optional;
-import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -21,8 +20,7 @@ public class FooServiceTest {
 
     @Test
     void serviceTest() {
-        String beansActual = Arrays.stream(context.getBeanDefinitionNames()).filter(arg -> arg.equals( "fooFormatter")).findFirst().get();
+        String beansActual = Arrays.stream(context.getBeanDefinitionNames()).filter(arg -> arg.equals("fooFormatter")).findFirst().get();
         assertEquals(beansActual, "fooFormatter");
-
     }
 }
